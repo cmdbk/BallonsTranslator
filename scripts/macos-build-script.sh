6a7344cf47c7bc80496cc1b1e6c027275d41b575
cmdbk <151101769+cmdbk@users.noreply.github.com>
2024-04-10 03:32:12 +0800

    Update macos-build-script.sh

--- scripts/macos-build-script.sh
+++ scripts/macos-build-script.sh
@@ -1,12 +1,3 @@
-# Define directories
-DATA_DIR='data'
-LIBS_DIR='data/libs'
-MODELS_DIR='data/models'
-MANGA_OCR_BASE_DIR='data/models/manga-ocr-base'
-PKUSEG_DIR='data/models/pkuseg'
-POSTAG_DIR='data/models/pkuseg/postag'
-SPACY_ONTONOTES_DIR='data/models/pkuseg/spacy_ontonotes'
-
 # Create and activate Python virtual environment
 echo "STEP 2: Create and activate Python virtual environment"
 python_version=$(python3 -V 2>&1 | cut -d" " -f2 | cut -d"." -f1-2)
@@ -28,65 +19,6 @@ else
         echo "ERROR: Please switch to Python 3.11 before running this script."
         exit 1
     fi
-fi
-
-# OpenCV installation check
-echo "STEP 3: Check installation of OpenCV."
-echo "INFO: Install OpenCV Python package in virtual environment."
-pip3 install opencv-python
-echo "INFO: Checking OpenCV installation..."
-python3 -c "import cv2" 2>/dev/null
-if [ $? -eq 0 ]; then
-    opencv_version=$(python3 -c "import cv2; print(cv2.__version__)")
-    echo "INFO: ✅ OpenCV is installed. Version: $opencv_version"
-else
-    echo "ERROR: ❌ OpenCV is not installed."
-    echo "ERROR: Please install OpenCV before running this script."
-    echo "INFO: Recommand install via Homebrew with command 'brew install opencv'."
-    exit 1
-fi
-
-# Checklist of extra data files
-check_list="
-data/alphabet-all-v5.txt
-$LIBS_DIR/libopencv_world.4.4.0.dylib
-$LIBS_DIR/libpatchmatch_inpaint.dylib
-$MODELS_DIR/aot_inpainter.ckpt
-$MODELS_DIR/comictextdetector.pt
-$MODELS_DIR/comictextdetector.pt.onnx
-$MODELS_DIR/lama_mpe.ckpt
-$MANGA_OCR_BASE_DIR/README.md
-$MANGA_OCR_BASE_DIR/config.json
-$MANGA_OCR_BASE_DIR/preprocessor_config.json
-$MANGA_OCR_BASE_DIR/pytorch_model.bin
-$MANGA_OCR_BASE_DIR/special_tokens_map.json
-$MANGA_OCR_BASE_DIR/tokenizer_config.json
-$MANGA_OCR_BASE_DIR/vocab.txt
-$MODELS_DIR/mit32px_ocr.ckpt
-$MODELS_DIR/mit48pxctc_ocr.ckpt
-$POSTAG_DIR/features.pkl
-$POSTAG_DIR/weights.npz
-$SPACY_ONTONOTES_DIR
-$SPACY_ONTONOTES_DIR/features.msgpack
-$SPACY_ONTONOTES_DIR/weights.npz
-data/pkusegscores.json
-"
-
-# Validate extra data files exist
-echo "STEP 5: Validate data files exist."
-fail=false
-for item in $check_list; do
-    if [ ! -e "$item" ]; then
-        echo "ERROR: ❌ $item not found"
-        fail=true
-    fi
-done
- 
-if [ "$fail" = true ]; then
-    echo "ERROR: ❌ Data files check failed. Exiting."
-    exit 1
-else
-    echo "INFO: ✅ Data files all exist."
 fi
 
 # Install Python dependencies
